8251d54998921e1d273ece503122aca0131c8941
uchanjeon <uchanjeon@gmail.com>
2024-04-18 20:45:30 +0900

    좋아요 버튼 홈페이지에서 구현

--- static/function.js
+++ static/function.js
@@ -159,6 +159,37 @@ $("#four4").click(async function () {
 
 
 });
+
+document.querySelectorAll(".post").forEach(post => {
+    const postId = post.dataset.postId;
+    const ratings = post.querySelectorAll(".post-rating");
+    const likeRating = ratings[0];
+
+    ratings.forEach(rating => {
+        const button = rating.querySelector(".post-rating-button");
+        const count = rating.querySelector(".post-rating-count");
+
+        button.addEventListener("click", async () => {
+
+            rating.classList.add("post-rating-selected");
+
+            ratings.forEach(rating => {
+                if (rating.classList.contains("post-rating-selected")) {
+
+                    count.textContent = Math.max(0, Number(count.textContent) + 1);
+                    rating.classList.remove("post-rating-selected");
+                }
+            });
+
+
+            const likeOrDislike = likeRating === rating ? "like" : "dislike";
+            const response = await fetch(`/posts/${postId}/${likeOrDislike}`);
+            const body = await response.json();
+        });
+    });
+});
+
+
 /*
 $("#five5").click(async function () {
     if ($('#mainimg').css('display') === "block" && $('#fifth').css('display') === "block") {
